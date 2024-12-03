5cef931e671365286832be36300a348e5dd461ca
dvdek12 <przemekdudek321@gmail.com>
2024-12-03 14:07:10 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <p>Click on the Vite and React logos to learn more</p>
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
+}
+
+export default App;
